1597a6b4b994e9c09f612b99e76ff24c7aa40c48
John Anderson <john.anderson@flextronics.com>
2016-06-20 20:38:33 -0400

    HubV2: Resize MTD partitions for more rootfs space
    Proof of concept for changing partition sizes; confirmed working.  This
    won't be the final layout, because there's no space for a database
    partition, nor an updater kernel/DTB/rootfs.

--- include/configs/mx6ul_flex_winkhub2.h
+++ include/configs/mx6ul_flex_winkhub2.h
@@ -135,7 +135,7 @@
 
 #define CONFIG_SYS_MMC_IMG_LOAD_PART	1
 #ifdef CONFIG_SYS_BOOT_NAND
-#define CONFIG_MFG_NAND_PARTITION "mtdparts=gpmi-nand:64m(boot),16m(kernel),16m(dtb),-(rootfs) "
+#define CONFIG_MFG_NAND_PARTITION "mtdparts=gpmi-nand:4m(boot),11m(kernel),1m(dtb),-(rootfs) "
 #else
 #define CONFIG_MFG_NAND_PARTITION ""
 #endif
@@ -169,9 +169,9 @@
 	"console=ttymxc0\0" \
 	"bootargs=console=ttymxc0,115200 ubi.mtd=3 "  \
 		"root=ubi0:rootfs rootfstype=ubifs "		     \
-		"mtdparts=gpmi-nand:64m(boot),16m(kernel),16m(dtb),-(rootfs)\0"\
-	"bootcmd=nand read ${loadaddr} 0x4000000 0x800000;"\
-		"nand read ${fdt_addr} 0x5000000 0x100000;"\
+		"mtdparts=gpmi-nand:4m(boot),11m(kernel),1m(dtb),-(rootfs)\0"\
+	"bootcmd=nand read ${loadaddr} 0x400000 0xb00000;"\
+		"nand read ${fdt_addr} 0xf00000 0x100000;"\
 		"bootz ${loadaddr} - ${fdt_addr}\0"
 
 #else
