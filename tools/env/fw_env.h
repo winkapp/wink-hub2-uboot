11ef456ce55a771913ab0ef2acc7d539df45d52b
John Anderson <john.anderson@flextronics.com>
2016-07-29 15:45:33 -0400

    Point fw_env.h at HubV2 environment area

--- tools/env/fw_env.h
+++ tools/env/fw_env.h
@@ -20,20 +20,20 @@
  * See included "fw_env.config" sample file
  * for notes on configuration.
  */
-#define CONFIG_FILE     "/etc/fw_env.config"
+// #define CONFIG_FILE     "/etc/fw_env.config"
 
 #ifndef CONFIG_FILE
-#define HAVE_REDUND /* For systems with 2 env sectors */
-#define DEVICE1_NAME      "/dev/mtd1"
-#define DEVICE2_NAME      "/dev/mtd2"
-#define DEVICE1_OFFSET    0x0000
-#define ENV1_SIZE         0x4000
-#define DEVICE1_ESIZE     0x4000
-#define DEVICE1_ENVSECTORS     2
-#define DEVICE2_OFFSET    0x0000
-#define ENV2_SIZE         0x4000
-#define DEVICE2_ESIZE     0x4000
-#define DEVICE2_ENVSECTORS     2
+// #define HAVE_REDUND /* For systems with 2 env sectors */
+#define DEVICE1_NAME      "/dev/mtd0"
+// #define DEVICE2_NAME      "/dev/mtd2"
+#define DEVICE1_OFFSET    0x2e0000
+#define ENV1_SIZE         0x20000
+#define DEVICE1_ESIZE     0x20000
+#define DEVICE1_ENVSECTORS     1
+// #define DEVICE2_OFFSET    0x2e0000
+// #define ENV2_SIZE         0x20000
+// #define DEVICE2_ESIZE     0x20000
+// #define DEVICE2_ENVSECTORS     1
 #endif
 
 #ifndef CONFIG_BAUDRATE
